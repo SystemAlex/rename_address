f6e6020435418f91ed2caaabf6bc148d87fb5381
Francisco Beese <alexandrobeese@hotmail.com>
2025-04-14 05:42:23 -0300

    Perform Cleanup

--- options.js
+++ options.js
@@ -1,22 +1,22 @@
-function saveOptions() {
-  const fullscreenEnabled = document.getElementById("fullscreenToggle").checked;
-  const toastEnabled = document.getElementById("toastToggle").checked;
-  chrome.storage.sync.set({ fullscreenEnabled, toastEnabled }, function () {
-    showToast("Opciones guardadas!");
-  });
+﻿function saveOptions() {
+    const fullscreenEnabled = document.getElementById("fullscreenToggle").checked;
+    const toastEnabled = document.getElementById("toastToggle").checked;
+    chrome.storage.sync.set({ fullscreenEnabled, toastEnabled }, function () {
+        showToast("Opciones guardadas!");
+    });
 }
 
 // Función para restaurar opciones al cargar la página
 function restoreOptions() {
-  chrome.storage.sync.get(
-    ["fullscreenEnabled", "toastEnabled"],
-    function (data) {
-      document.getElementById("fullscreenToggle").checked =
-        data.fullscreenEnabled || false;
-      document.getElementById("toastToggle").checked =
-        data.toastEnabled || false;
-    }
-  );
+    chrome.storage.sync.get(
+        ["fullscreenEnabled", "toastEnabled"],
+        function (data) {
+            document.getElementById("fullscreenToggle").checked =
+                data.fullscreenEnabled || false;
+            document.getElementById("toastToggle").checked =
+                data.toastEnabled || false;
+        }
+    );
 }
 
 // Cargar las opciones al iniciar la página
